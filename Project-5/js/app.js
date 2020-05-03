d868284f1c0516b8d9f28c88ccddfdff91670baf
majadivkovic <61597071+majadivkovic@users.noreply.github.com>
2020-05-04 01:16:34 +0200

    adding project 5

---
+++ Project-5/js/app.js
@@ -0,0 +1,25 @@
+$('[data-fancybox="gallery"]').fancybox({
+	loop:true,
+	animationDuration: 150,
+	animationEffect: "fade",
+	buttons: [
+		//"zoom",
+		//"share",
+		//"slideShow",
+		//"fullScreen",
+		//"download",
+		//"thumbs",
+		"close"
+	  ],
+});
+
+const $fuzzy = $('.fuzzy');
+const $images = $('img')
+
+$fuzzy.on("keyup search", function(evt) {
+	const value = $fuzzy.val().toLowerCase();
+	$images.parent().show();
+	if (value !== '') {
+	  $images.not('[data-caption*="' + value + '"]').parent().hide();
+	}
+});
